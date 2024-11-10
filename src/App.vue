0c0abd33e30bbf6fbdf3a118d0cc1ac4bc0d3e5e
藤岡和弥 <kfujioka@fujiokakazuyanoMacBook-Air.local>
2024-11-10 18:44:22 +0900

    implement tic tac toe

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,165 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <Scatter :data="data" :options="options" width="400" height="400" />
+  </div>
 </template>
+<script setup lang="ts">
+import { Chart, registerables } from 'chart.js';
+import type { ChartData, ChartOptions } from 'chart.js';
+import { computed, ref } from 'vue';
+import { Scatter } from 'vue-chartjs';
 
-<style scoped>
-header {
-  line-height: 1.5;
+Chart.register(...registerables);
+
+type CellIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+const unFilledCells = ref<CellIndex[]>([0, 1, 2, 3, 4, 5, 6, 7, 8])
+const player1Cells = ref<CellIndex[]>([])
+const player2Cells = ref<CellIndex[]>([])
+const player1Color = 'rgb(54, 162, 235)'
+const player2Color = 'rgb(255, 99, 132)'
+
+const winningPlayer = computed(() => {
+  if (getWinningCombo(player1Cells.value)) {
+    return 'player1'
+  }
+  if (getWinningCombo(player2Cells.value)) {
+    return 'player2'
+  }
+  if (unFilledCells.value.length === 0) {
+    return 'draw'
+  }
+  return undefined
+})
+
+const winningCombo = computed(() => {
+  return winningPlayer.value === 'player1' ? getWinningCombo(player1Cells.value) : getWinningCombo(player2Cells.value)
+})
+
+const nextPlayer = computed(() => {
+  return player1Cells.value.length === player2Cells.value.length ? 'player1' : 'player2'
+})
+
+function cellIndexToCoord(cellIndex: CellIndex): { x: number, y: number } {
+  return {
+    x: cellIndex % 3 + 0.5,
+    y: Math.floor(cellIndex / 3) + 0.5,
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+function getWinningCombo(cells: CellIndex[]) {
+  const winningCombos: [CellIndex, CellIndex, CellIndex][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  return winningCombos.find((combo) => {
+    return combo.every((cell) => cells.includes(cell))
+  })
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+const data = computed<ChartData<"scatter">>(() => ({
+  datasets: [
+    {
+      label: 'unFilledCells',
+      data: unFilledCells.value.map(cellIndexToCoord),
+      pointRadius: 20,
+      pointHoverRadius: 20,
+      backgroundColor: 'transparent',
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+      borderWidth: 2,
+      hoverBorderWidth: 2,
+      hoverBorderColor: winningPlayer.value === undefined ? nextPlayer.value === 'player1' ? player1Color : player2Color : (ctx) => ctx.chart.data.datasets[0].borderColor,
+      pointStyle: nextPlayer.value === 'player1' ? 'circle' : nextPlayer.value === 'player2' ? 'crossRot' : ''
+    },
+    {
+      label: 'Player 1',
+      data: player1Cells.value.map(cellIndexToCoord),
+      pointStyle: 'circle',
+      backgroundColor: 'transparent',
+      borderColor: winningPlayer.value === undefined ? player1Color : winningPlayer.value === 'player1' ? player1Cells.value.map(i => winningCombo.value?.includes(i) ? player1Color : 'rgba(0, 0, 0, 0.3)') : 'rgba(0, 0, 0, 0.3)',
+      borderWidth: 2,
+      hoverBorderWidth: 2,
+      pointRadius: 20,
+      pointHoverRadius: 20,
+    },
+    {
+      label: 'Player 2',
+      data: player2Cells.value.map(cellIndexToCoord),
+      pointStyle: 'crossRot',
+      backgroundColor: 'transparent',
+      borderColor: winningPlayer.value === undefined ? player2Color :winningPlayer.value === 'player2' ? player2Cells.value.map(i => winningCombo.value?.includes(i) ? player2Color : 'rgba(0, 0, 0, 0.3)') : 'rgba(0, 0, 0, 0.3)',
+      borderWidth: 2,
+      hoverBorderWidth: 2,
+      pointRadius: 20,
+      pointHoverRadius: 20,
+    },
+  ],
+}));
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+const options = computed<ChartOptions<"scatter">>(() => ({
+  animation: false,
+  onClick: (e, elements) => {
+    const elemsOfunFilledCell = elements.filter((element) => element.datasetIndex === 0)
+    if (winningPlayer.value !== undefined || elemsOfunFilledCell.length === 0) {
+      return
+    }
+    const cellIndex = unFilledCells.value[elements[0].index]
+    if (nextPlayer.value === 'player1') {
+      player1Cells.value.push(cellIndex)
+    } else {
+      player2Cells.value.push(cellIndex)
+    }
+    unFilledCells.value = unFilledCells.value.filter((cell) => cell !== cellIndex)
+  },
+  plugins: {
+    title: {
+      display: true,
+      text: 'Tic Tac Toe',
+    },
+    tooltip: {
+      enabled: false,
+    },
+    legend: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      max: 3,
+      min: 0,
+      type: 'linear',
+      position: 'bottom',
+      ticks: {
+        display: false,
+        maxTicksLimit: 4,
+      },
+      title: {
+        display: true,
+        text: "O Player 1" + (winningPlayer.value === "player1" ? " Wins!!" : ""),
+        color: winningPlayer.value === undefined ? nextPlayer.value === 'player1' ? player1Color : 'rgba(0, 0, 0, 0.3)' : winningPlayer.value === 'player1' ? player1Color : 'rgba(0, 0, 0, 0.3)',
+      }
+    },
+    y: {
+      max: 3,
+      min: 0,
+      type: 'linear',
+      position: 'bottom',
+      ticks: {
+        maxTicksLimit: 4,
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "X Player 2" + (winningPlayer.value === "player2" ? " Wins!!" : ""),
+        color: winningPlayer.value === undefined ? nextPlayer.value === 'player2' ? player2Color : 'rgba(0, 0, 0, 0.3)' : winningPlayer.value === 'player2' ? player2Color : 'rgba(0, 0, 0, 0.3)',
+      }
+    },
+  },
+}));
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
